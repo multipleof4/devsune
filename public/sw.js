1a6a24dc80731011b5bb5203ca38ef66e02d031e
multipleof4 <planetrenox@protonmail.com>
2025-08-22 21:13:08 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,19 @@
+const log = (...a) => console.log('[SW]', ...a)
 
+self.addEventListener('install', e => {
+  log('install')
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  log('activate')
+  e.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') log('navigate', e.request.url)
+})
+
+self.addEventListener('message', e => {
+  log('message', e.data)
+})
